d02ff1c8ceed4ed154b4c900354f0ed259a520b1
chasscepts <francischassobetta@gmail.com>
2021-03-30 12:31:36 +0100

    add spec for Enumerable::my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,7 +2,7 @@ require_relative '../enumerables.rb'
 
 describe Enumerable do
   describe 'my_each method' do
-    it 'returns empty array when collection is empty' do
+    it 'yields no item when collection is empty' do
       expected = []
       [].my_each { |item| expected << item }
       expect(expected).to eq([])
@@ -21,8 +21,32 @@ describe Enumerable do
       expect(expected).to eq([1, 2, 3])
     end
 
-    it 'returns an Enumerator when block is not passed in' do
+    it 'returns an Enumerator when block is not given' do
       expect([1, 2, 3].my_each.class).to eq(Enumerator)
+    end
+  end
+
+  describe 'my_each_with_index method' do
+    it 'yields no item when collection is empty' do
+      expected = {}
+      [].my_each_with_index { |item, index| expected[index.to_s] = item }
+      expect(expected).to eq({})
+    end
+
+    it "yields each item (with it's index) in an array when array is NOT empty" do
+      expected = {}
+      [1, 2, 3].my_each_with_index { |item, index| expected[index.to_s] = item}
+      expect(expected).to eq({ '0' => 1, '1' => 2, '2' => 3 })
+    end
+
+    it "yields each item (with it's index) in a given range" do
+      expected = {}
+      (1..3).my_each_with_index { |item, index| expected[index.to_s] = item }
+      expect(expected).to eq({ '0' => 1, '1' => 2, '2' => 3 })
+    end
+
+    it 'returns an Enumerator when block is not given' do
+      expect([1, 2, 3].my_each_with_index.class).to eq(Enumerator)
     end
   end
 end
